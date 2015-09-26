d9165d07b07f977e9bd82d6ff15ff6d14bdc8d01
Vladislav Baimurzin <baimurzinv@gmail.com>
2015-09-26 14:31:47 +0300

    removed commented code

--- app/report/chart/chart.controller.js
+++ app/report/chart/chart.controller.js
@@ -7,36 +7,21 @@ angular
 
 ChartReportController.$inject = ['$scope', 'ChartService'];
 function ChartReportController($scope, ChartService) {
-    $scope.labels = []; // название таксков
-    $scope.series = []; // на кого из пользователей составляется график
-    $scope.data = []; // добавить сюда массив с данными для каждой задачи
+    $scope.labels = [];
+    $scope.series = [];
+    $scope.data = [];
     var mapTooltips = new Map();
     $scope.options = {
-        // String - Template string for single tooltips
         tooltipTemplate: function(valuesObject){
             var tooltip = mapTooltips.get(valuesObject.label);
-            // do different things here based on whatever you want;
             return "task point: "+ tooltip.totalPoint + "\n" + "max point: " + tooltip.maxPoint + "\n" + "summary: " + tooltip.summary;
         },
-        // String - Template string for multiple tooltips
         multiTooltipTemplate: "<%= value + ' %' %>"
     };
-    //initialize();
     $scope.onClick = function (points, evt) {
-        //do smth
     };
-
-    //function initialize() {
-    //    debugger;
-    //    var data = ChartService.getData();
-    //    $scope.series = data.assignee;
-    //    data.issue.forEach(function (val) {
-    //        $scope.labels = val.id;
-    //        $scope.data = val.totalPoint;
-    //    });
-    //}
     init();
-    //testData();
+
     function init() {
         var d = ChartService.getData();
         $scope.series.push(d.assignee);
@@ -53,21 +38,4 @@ function ChartReportController($scope, ChartService) {
         $scope.labels = labels;
         $scope.data.push(data);
     }
-
-    function testData() {
-        $scope.series.push('TEST');
-        var labels = [];
-        for (var i = 1; i < 21; i++) {
-            labels.push('TASK-' + i);
-        }
-        $scope.labels = labels;
-        var data = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 100];
-        $scope.data.push(data);
-    }
-
-    //$scope.$on('incomingChartData', function () {
-    //    var data = ChartService.getData();
-    //    console.log(data);
-    //    console.log("EVENT")
-    //});
 }
